369fd64c6f6aea4f2372c08e5677d7184c2915c8
Kranthi reddy <kranthi.r@optit.in>
2023-11-06 09:57:24 +0530

    changed backend file

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,13 @@
 terraform {
-  backend "s3" {
-    bucket = "sandip-demo-tfstate-bucket"
-    key    = "eks/terraform.tfstate"
-    region = "us-west-2"
+
+  backend "local" {
+    path = "./terraform.tfstate"
   }
+  # backend "s3" {
+  #   bucket = "sandip-demo-tfstate-bucket"
+  #   key    = "eks/terraform.tfstate"
+  #   region = "us-west-2"
+  # }
 }
 
 
