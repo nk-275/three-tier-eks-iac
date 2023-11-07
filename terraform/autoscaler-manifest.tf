7c612509075ed3b4d27b5b155e3f7ca66c40024e
Ubuntu <ubuntu@ip-172-31-17-236.us-west-2.compute.internal>
2023-11-07 09:44:30 +0000

    ingress changes

--- terraform/autoscaler-manifest.tf
+++ terraform/autoscaler-manifest.tf
@@ -1,7 +1,7 @@
 provider "kubectl" {
   host                   = data.aws_eks_cluster.default.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
-  load_config_file       = false
+  load_config_file       = false 
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
