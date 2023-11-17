735c99106fcf798112eb8e5ed6a24c84d4d2b898
Kranthi reddy <kranthi.r@optit.in>
2023-11-17 11:09:57 +0530

    montring changes

--- terraform/monitoring/main.tf
+++ terraform/monitoring/main.tf
@@ -1,6 +1,9 @@
+
 resource "kubernetes_manifest" "elastic_dashboard" {
 manifest = yamldecode(templatefile("./elastic-dashboard-configmap.yaml", {
   name = "grafana-elastic"
   data = file("./grafana-elastic-dashboard.json")
 }))
 }
+
+
